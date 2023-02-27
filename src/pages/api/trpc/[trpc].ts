369d0a158ddc664ea69db6043e7f0d033199a26d
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-02-27 13:48:06 -0300

    init trpc + prisma

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,9 @@
+import * as trpcNext from '@trpc/server/adapters/next'
+
+import { createContext } from '@server/context'
+import { serverRouter } from '@server/router'
+
+export default trpcNext.createNextApiHandler({
+  router: serverRouter,
+  createContext,
+})
