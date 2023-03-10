1386ea6ea1ef930078da7ec6279d23d8f4e67c5a
moises lopes ferreira <moises.ferreira@scientificgames.com>
2023-03-10 11:28:15 -0300

    fix: refactor error handling in uploadToCloudinary function

--- src/pages/api/upload-pdf.ts
+++ src/pages/api/upload-pdf.ts
@@ -1,141 +1,104 @@
-import extractPdfData from '@/libs/extractPdfData'
-import uploadToCloudinary from '@/libs/uploadToCloudinary'
-import fsExtra, { mkdir } from 'fs-extra'
-import multer, { MulterError } from 'multer'
-import { NextApiRequest, NextApiResponse } from 'next'
-import path from 'path'
-import { PDFDocument } from 'pdf-lib'
-import { v4 as uuidv4 } from 'uuid'
+import extractPdfData from '@libs/extractPdfData';
+import uploadToCloudinary from '@libs/uploadToCloudinary';
+import cloudinary from 'cloudinary';
+import multer, { MulterError } from 'multer';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PDFDocument } from 'pdf-lib';
 
-const storage = multer.diskStorage({
-  destination: path.join(process.cwd(), 'public', 'uploads'),
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-    cb(null, `${uuidv4()}-${uniqueSuffix}`)
-  },
-})
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const upload = multer({ storage }).array('pdf')
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage }).array('pdf');
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   upload(req, res, async (err: MulterError) => {
     if (err) {
-      console.error(err)
-      res.status(500).send('Error uploading files')
-      return
+      console.error(err);
+      res.status(500).send('Error uploading files');
+      return;
     }
-
     try {
-      // Get uploaded files
-      const uploadedFiles = req.files as Express.Multer.File[]
+      const uploadedFiles = req.files as Express.Multer.File[];
 
-      // Extract data from each PDF file and upload to Cloudinary
       const results = await Promise.all(
-        uploadedFiles.map(async (file: Express.Multer.File) => {
+        uploadedFiles.map(async (file) => {
           try {
-            if (!file.path) {
+            if (!file.buffer) {
               return {
                 fileName: file.originalname,
-                error: 'File path not found',
-              }
+                error: 'File buffer not found',
+              };
             }
 
-            // Read PDF file
-            const pdfBuffer = await fsExtra.readFile(file.path)
+            const pdfData = file.buffer;
+            const pdfDoc = await PDFDocument.load(pdfData);
+            const pageBuffers: Buffer[] = [];
 
-            const splitPdfIntoPages = async (
-              pdfBuffer: Buffer
-            ): Promise<string[]> => {
-              const pdfDoc = await PDFDocument.load(pdfBuffer)
-              const pageBase64Strings: string[] = []
+            await Promise.all(
+              pdfDoc.getPages().map(async (_, index) => {
+                const newDocument = await PDFDocument.create();
+                const [copiedPage] = await newDocument.copyPages(pdfDoc, [index]);
+                newDocument.addPage(copiedPage);
+                const pageBuffer = await newDocument.save();
+                pageBuffers.push(pageBuffer);
+              })
+            );
 
-              await Promise.all(
-                pdfDoc.getPages().map(async (_, index) => {
-                  const newDocument = await PDFDocument.create()
-                  const [copiedPage] = await newDocument.copyPages(pdfDoc, [
-                    index,
-                  ])
-                  newDocument.addPage(copiedPage)
-                  pageBase64Strings.push(await newDocument.saveAsBase64())
-                })
-              )
+            let uploadedPages = 0;
+            const cloudinaryFolder = 'pdf-uploads';
+            await Promise.all(
+              pageBuffers.map(async (pageBuffer) => {
+                const extractedData = await extractPdfData(pageBuffer);
 
-              await Promise.all(
-                pageBase64Strings.map(async (pageBase64String) => {
-                  const pageBuffer = Buffer.from(pageBase64String, 'base64')
-                  const extractedData = await extractPdfData(pageBuffer)
-                  
-                  if (extractedData) {
-                    extractedData.map(async (data) => {
-                      const pageFileName = extractedData.map((data) => `${data.cpf}-${data.name}-${data.enrollment}-${data.month}-${data.year}.pdf`).toString()
-                      if (data.cpf && data.name && data.enrollment && data.month && data.year) {
-                        // verify if folder exists
-                        if (!fsExtra.existsSync(path.join(process.cwd(), 'public', 'uploads', `${data.year}`))){
-                          mkdir(path.join(process.cwd(), 'public', 'uploads', `${data.year}`))
-                      }  
-                          const pageFolderPath = path.join(
-                            process.cwd(),
-                            'public',
-                            'uploads',
-                            `${data.year}`
-                          )
-                          uploadToCloudinary(pageBuffer, pageFileName, pageFolderPath)
-                          await fsExtra.writeFile(
-                            path.join(pageFolderPath, pageFileName),
-                            pageBuffer
-                          )
-                        
-                      } else {
-                        if (!fsExtra.existsSync(path.join(process.cwd(), 'public', 'uploads', `${data.year}-with-errors`))){
-                          mkdir(path.join(process.cwd(), 'public', 'uploads', `${data.year}-with-errors`))
-                        }
+                if (extractedData[0]) {
+                  const { cpf, year, month, name, enrollment } = extractedData[0];
+                  const pageFileName = `${cpf}-${name}-${enrollment}-${month}-${year}.pdf`;
 
-                        const pageFolderPath = path.join(
-                          process.cwd(),
-                          'public',
-                          'uploads',
-                          `${data.year}_with-errors`
-                        )
-                        await fsExtra.writeFile(
-                          path.join(pageFolderPath, pageFileName),
-                          pageBuffer
-                        )
-                      }
-                      
-                       // Remove uploaded file from server
-                       await fsExtra.remove(file.path)
-                    }
-                    )
+                  const fileExists = await cloudinary.search
+                    .expression(`folder:${cloudinaryFolder}/${cpf}/${year}/${month} AND filename:${pageFileName}`)
+                    .execute()
+                    .then((result) => result.total_count > 0);
+
+                  if (fileExists) {
+                    console.log(`File ${pageFileName} already exists, skipping upload`);
+                  } else {
+                    const uploadResponse = await uploadToCloudinary(pageBuffer, pageFileName, `${cloudinaryFolder}/${cpf}/${year}/${month}`, (progress) => {
+                      uploadedPages += 1;
+                      const percentage = Math.round((uploadedPages / pageBuffers.length) * 100);
+                      console.log(`Page ${uploadedPages} of ${pageBuffers.length} uploaded: ${percentage}%`);
+                    });
+                    console.log(`File ${pageFileName} uploaded successfully to ${uploadResponse.url}`);
                   }
-                })
-              )
+                }
+              })
+            );
 
-              return pageBase64Strings
-            }
-
-            // split PDF upload by page and save to public/uploads folder
-            const pageBuffers = await splitPdfIntoPages(pdfBuffer)
-
-            return { pageBuffers }
+            return { fileName: file.originalname, numPages: pageBuffers.length };
           } catch (error) {
-            console.error(error)
-            return { fileName: file.originalname, error: error.message }
+            console.error(error);
+            return { fileName: file.originalname, error: error.message };
           }
         })
-      )
+      );
 
-      res.status(200).json({ results })
+      res.status(200).json({ results });
     } catch (error) {
-      console.error(error)
-      res.status(500).send('Error processing files')
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
     }
   })
 }
+
 
 export const config = {
   api: {
     bodyParser: false,
   },
-}
+};
 
 export default handler
--- src/types/pdf-extraction.d.ts
+++ src/types/pdf-extraction.d.ts
@@ -1,17 +1,17 @@
 declare module 'pdf-extraction' {
-const pdf: (buffer: Buffer) => Promise<{
-    text: string;
-    metadata: any;
-    version: string;
-    info: any;
+  const pdf: (buffer: Buffer) => Promise<{
+    text: string
+    metadata: any
+    version: string
+    info: any
     pages: {
-    pageId: number;
-    text: string;
-    width: number;
-    height: number;
-    metadata: any;
-    }[];
-}>;
+      pageId: number
+      text: string
+      width: number
+      height: number
+      metadata: any
+    }[]
+  }>
 
-export default pdf;
+  export default pdf
 }
