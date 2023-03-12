b222db9230ab93343988dc14508c5992bc924a70
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-03-12 12:03:55 -0300

    Auto stash before checking out "origin/develop"

--- src/pages/api/upload-pdf.ts
+++ src/pages/api/upload-pdf.ts
@@ -1,104 +1,101 @@
-import extractPdfData from '@libs/extractPdfData';
-import uploadToCloudinary from '@libs/uploadToCloudinary';
-import cloudinary from 'cloudinary';
-import multer, { MulterError } from 'multer';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PDFDocument } from 'pdf-lib';
-
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const storage = multer.memoryStorage();
-
-const upload = multer({ storage }).array('pdf');
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  upload(req, res, async (err: MulterError) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error uploading files');
-      return;
-    }
-    try {
-      const uploadedFiles = req.files as Express.Multer.File[];
-
-      const results = await Promise.all(
-        uploadedFiles.map(async (file) => {
-          try {
-            if (!file.buffer) {
-              return {
-                fileName: file.originalname,
-                error: 'File buffer not found',
-              };
-            }
-
-            const pdfData = file.buffer;
-            const pdfDoc = await PDFDocument.load(pdfData);
-            const pageBuffers: Buffer[] = [];
-
-            await Promise.all(
-              pdfDoc.getPages().map(async (_, index) => {
-                const newDocument = await PDFDocument.create();
-                const [copiedPage] = await newDocument.copyPages(pdfDoc, [index]);
-                newDocument.addPage(copiedPage);
-                const pageBuffer = await newDocument.save();
-                pageBuffers.push(pageBuffer);
-              })
-            );
-
-            let uploadedPages = 0;
-            const cloudinaryFolder = 'pdf-uploads';
-            await Promise.all(
-              pageBuffers.map(async (pageBuffer) => {
-                const extractedData = await extractPdfData(pageBuffer);
-
-                if (extractedData[0]) {
-                  const { cpf, year, month, name, enrollment } = extractedData[0];
-                  const pageFileName = `${cpf}-${name}-${enrollment}-${month}-${year}.pdf`;
-
-                  const fileExists = await cloudinary.search
-                    .expression(`folder:${cloudinaryFolder}/${cpf}/${year}/${month} AND filename:${pageFileName}`)
-                    .execute()
-                    .then((result) => result.total_count > 0);
-
-                  if (fileExists) {
-                    console.log(`File ${pageFileName} already exists, skipping upload`);
-                  } else {
-                    const uploadResponse = await uploadToCloudinary(pageBuffer, pageFileName, `${cloudinaryFolder}/${cpf}/${year}/${month}`, (progress) => {
-                      uploadedPages += 1;
-                      const percentage = Math.round((uploadedPages / pageBuffers.length) * 100);
-                      console.log(`Page ${uploadedPages} of ${pageBuffers.length} uploaded: ${percentage}%`);
-                    });
-                    console.log(`File ${pageFileName} uploaded successfully to ${uploadResponse.url}`);
-                  }
-                }
-              })
-            );
-
-            return { fileName: file.originalname, numPages: pageBuffers.length };
-          } catch (error) {
-            console.error(error);
-            return { fileName: file.originalname, error: error.message };
-          }
-        })
-      );
-
-      res.status(200).json({ results });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  })
-}
-
+import extractPdfData from '@/libs/extractPdfData'
+import multer from 'multer'
+import { PDFDocument } from 'pdf-lib'
 
 export const config = {
   api: {
     bodyParser: false,
   },
-};
+}
 
-export default handler
+const storage = multer.memoryStorage()
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10, // limit file size to 10MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'application/pdf') {
+      cb(new Error('Invalid file type. Only PDF files are allowed.'))
+    } else {
+      cb(null, true)
+    }
+  },
+})
+
+export default async function handler(req, res) {
+  upload.single('pdf')(req, res, async (err) => {
+    if (err) {
+      console.error(`Error uploading file: ${err}`)
+      return res.status(500).send('Error uploading file')
+    }
+
+    const pdfData = req.file?.buffer
+    if (!pdfData) {
+      return res.status(400).send('No file uploaded')
+    }
+
+    const pdfDoc = await PDFDocument.load(pdfData)
+    const pageBuffers: Uint8Array[] = []
+
+    try {
+      await Promise.all(
+        pdfDoc.getPages().map(async (_, index) => {
+          const newDocument = await PDFDocument.create()
+          const [copiedPage] = await newDocument.copyPages(pdfDoc, [index])
+          newDocument.addPage(copiedPage)
+          const pageBuffer = await newDocument.save()
+          pageBuffers.push(pageBuffer)
+        })
+      )
+
+      const arrayResponseJson = [];
+
+      for (const pageBuffer of pageBuffers) {
+        try {
+          const extractedData = await extractPdfData(pageBuffer as Buffer)
+      
+          if (extractedData[0]) {
+            const { cpf, year, month, name, enrollment } = extractedData[0]
+            if (!cpf || !name || !enrollment) {
+              console.error('Invalid data extracted from PDF')
+              return res.status(400).send('Invalid input')
+            }
+      
+            const pageFileName = `${cpf}-${name}-${enrollment}-${month}-${year}.pdf`
+                  
+            if (!cpf) {
+              console.error('CPF is null or undefined')
+              return res.status(400).send('Invalid input')
+            }
+      
+            const pdfObject = {
+              cpf,
+              name,
+              enrollment,
+              year,
+              month,
+              pdf: {
+                fileName: pageFileName,
+                file: Buffer.from(pageBuffer).toString('base64'),
+              },
+            };
+            arrayResponseJson.push(pdfObject);
+          }
+        } catch (error) {
+          console.error(`Error processing PDF: ${error}`);
+          return res.status(500).send(`Error processing PDF: ${error}`);
+        }
+      }
+
+      // return in json format the array of objects
+      return res.status(200).json({
+        results: arrayResponseJson
+      })
+    } catch (error) {
+      console.error(`Error processing PDF: ${error}`)
+      return res.status(500).send('Error processing PDF')
+    }
+  })
+}
--- src/server/router.ts
+++ src/server/router.ts
@@ -1,45 +1,50 @@
 import { initTRPC, TRPCError } from '@trpc/server'
 import { hash } from 'argon2'
 
-import { beneficiarySchema, signUpSchema } from '@common/validation/auth'
+import { beneficiaryCPFSchema, beneficiarySchema } from '@common/validation/auth'
+import { beneficiaryPdfFileSchema } from '@common/validation/pdf'
 import { IContext } from '@server/context'
+
+type PdfFileType = 'HOLERITE' | 'DESMOTRATIVO_ANUAL'
 
 const t = initTRPC.context<IContext>().create()
 
+
 export const serverRouter = t.router({
-  signup: t.procedure.input(signUpSchema).mutation(async ({ input, ctx }) => {
-    const { cpf, password } = input
+  // TODO: change to suport only admin users
+  // signup: t.procedure.input(signUpSchema).mutation(async ({ input, ctx }) => {
+  //   const { cpf, password } = input
 
-    const exists = await ctx.prisma.beneficiaryUser.findFirst({
-      where: { cpf },
-    })
+  //   const exists = await ctx.prisma.beneficiaryUser.findFirst({
+  //     where: { cpf },
+  //   })
 
-    if (exists) {
-      throw new TRPCError({
-        code: 'CONFLICT',
-        message: 'User already exists.',
-      })
-    }
+  //   if (exists) {
+  //     throw new TRPCError({
+  //       code: 'CONFLICT',
+  //       message: 'User already exists.',
+  //     })
+  //   }
 
-    const hashedPassword = await hash(password)
+  //   const hashedPassword = await hash(password)
 
-    const result = await ctx.prisma.beneficiaryUser.create({
-      data: {
-        cpf,
-        password: hashedPassword,
-      },
-    })
+  //   const result = await ctx.prisma.beneficiaryUser.create({
+  //     data: {
+  //       cpf,
+  //       password: hashedPassword,
+  //     },
+  //   })
 
-    return {
-      status: 201,
-      message: 'Account created successfully',
-      result: result.cpf,
-    }
-  }),
+  //   return {
+  //     status: 201,
+  //     message: 'Account created successfully',
+  //     result: result.cpf,
+  //   }
+  // }),
   createBeneficiary: t.procedure
     .input(beneficiarySchema)
     .mutation(async ({ input, ctx }) => {
-      const { cpf, password, name, type_beneficiary, enrollment } = input
+      const { cpf, name, type_beneficiary, enrollment } = input
 
       const exists = await ctx.prisma.beneficiaryUser.findFirst({
         where: { cpf },
@@ -52,7 +57,7 @@ export const serverRouter = t.router({
         })
       }
 
-      const hashedPassword = await hash(password)
+      const hashedPassword = await hash(enrollment.toString())
 
       const result = await ctx.prisma.beneficiaryUser.create({
         data: {
@@ -68,6 +73,87 @@ export const serverRouter = t.router({
         status: 201,
         message: 'Account created successfully',
         result: result.cpf,
+      }
+    }),
+  uploadPdf: t.procedure
+    .input(beneficiaryPdfFileSchema)
+    .mutation(async ({ input, ctx }) => {
+    const { cpf, fileName, fileType, year, month, file } = input
+
+    // Verify that the user is authorized to perform this action
+    // const isAdmin = await ctx.prisma.adminUser.findFirst({
+    //   where: { email: (ctx.session as any)?.user?.email ?? '' },
+    // })
+    // if (!isAdmin) {
+    //   throw new TRPCError({
+    //     code: 'UNAUTHORIZED',
+    //     message: 'You are not authorized to perform this action',
+    //   })
+    // }
+
+    // Convert the base64 string to a binary buffer
+    const fileBuffer = Buffer.from(file, 'base64')
+
+    // Create the PDF file in the database
+    await ctx.prisma.beneficiaryPdfFile.create({
+      data: {
+        cpf,
+        fileName,
+        fileType: fileType as PdfFileType,
+        year,
+        month,
+        file: fileBuffer,
+      }
+    })
+
+    return { success: true, error: null }
+  }),
+  verifyIfBeneficiaryExists: t.procedure
+    .input(beneficiaryCPFSchema)
+    .query(async ({ input, ctx }) => {
+      const { cpf } = input
+
+      const exists = await ctx.prisma.beneficiaryUser.findFirst({
+        where: { cpf },
+      })
+
+      if (!exists) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'User not found.',
+        })
+      }
+
+      return {
+        status: 200,
+        message: 'User found successfully',
+        result: exists.cpf,
+      }
+    }),
+  getBeneficiaryPdfFiles: t.procedure
+    .input(beneficiaryCPFSchema)
+    .query(async ({ input, ctx }) => {
+      const { cpf } = input
+
+      const exists = await ctx.prisma.beneficiaryUser.findFirst({
+        where: { cpf },
+      })
+
+      if (!exists) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'User not found.',
+        })
+      }
+
+      const pdfFiles = await ctx.prisma.beneficiaryPdfFile.findMany({
+        where: { cpf },
+      })
+
+      return {
+        status: 200,
+        message: 'User found successfully',
+        result: pdfFiles,
       }
     }),
 })
