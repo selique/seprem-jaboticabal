29488df4dbd8536978f6bc13df572035fb262240
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-03-27 15:43:09 -0300

    Update file type selection: use IFileType instead of literal types
    Allow enum value to be passed to setFileType()
    for improved type safety and reduced cognitive overhead.

--- src/pages/admin/upload-pdf.tsx
+++ src/pages/admin/upload-pdf.tsx
@@ -204,7 +204,7 @@ const UploadPdf: NextPage = () => {
         <select
           value={fileType}
           onChange={(e) =>
-            setFileType(e.target.value as 'HOLERITE' | 'DEMOSTRATIVO_ANUAL')
+            setFileType(e.target.value as IFileType)
           }
           className="block w-full px-4 py-2 text-base border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         >
