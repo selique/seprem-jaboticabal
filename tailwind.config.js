8ebb6a1e12b6ab2c88e8914538b22c0157ea1f21
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-04-19 09:35:39 -0300

    Add font size presets to tailwind config
    Extend font size presets in tailwind configuration
    with xs, sm, base, lg, xl, 2xl, 3xl, 4xl,
    5xl and 6xl aliases.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,20 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontSize: {
+        'xs': '0.9375rem', // 25% larger than .75rem
+        'sm': '1.09375rem', // 25% larger than .875rem
+        'base': '1.40625rem', // 25% larger than 1.125rem
+        'lg': '1.5625rem', // 25% larger than 1.25rem
+        'xl': '1.875rem', // 25% larger than 1.5rem
+        '2xl': '2.34375rem', // 25% larger than 1.875rem
+        '3xl': '2.8125rem', // 25% larger than 2.25rem
+        '4xl': '3.75rem', // 25% larger than 3rem
+        '5xl': '5rem', // 25% larger than 4rem
+        '6xl': '6.25rem' // 25% larger than 5rem
+      }
+    }
   },
   plugins: []
 }
