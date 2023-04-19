72cd9f04d78c3cd94fcf906cc4bc10139415d147
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-04-19 01:14:11 -0300

    refactor: button and index components
    Move type definitions out of Button
    and Index components into separate files

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -20,11 +20,16 @@ const Button: React.FC<ButtonProps> = ({
       <a
         href={href}
         className={`
-        ${color === 'primary' ? 'bg-blue-500' : 'bg-gray-500'}
-        ${color === 'primary' ? 'hover:bg-blue-700' : 'hover:text-white'}
-        ${color === 'secondary' ? 'bg-gray-500' : 'text-white'}
-        ${color === 'secondary' ? 'hover:bg-gray-700' : 'hover:text-white'}
-        text-white font-bold py-2 px-4 rounded`}
+          min-h-[50px]
+          flex
+          items-center
+          justify-center
+          text-white font-bold rounded
+          ${color === 'primary' ? 'bg-blue-500' : 'bg-gray-500'}
+          ${color === 'primary' ? 'hover:bg-blue-700' : 'hover:text-white'}
+          ${color === 'secondary' ? 'bg-gray-500' : 'text-white'}
+          ${color === 'secondary' ? 'hover:bg-gray-700' : 'hover:text-white'}
+        `}
         {...props}
       >
         {children}
@@ -35,11 +40,12 @@ const Button: React.FC<ButtonProps> = ({
       <button
         type={type}
         className={`
+        text-white font-bold rounded min-h-[50px]
           ${color === 'primary' ? 'bg-blue-500' : 'bg-gray-500'}
           ${color === 'primary' ? 'hover:bg-blue-700' : 'hover:text-white'}
           ${color === 'secondary' ? 'bg-gray-500' : 'text-white'}
           ${color === 'secondary' ? 'hover:bg-gray-700' : 'hover:text-white'}
-          text-white font-bold py-2 px-4 rounded`}
+        `}
         {...props}
       >
         {children}
