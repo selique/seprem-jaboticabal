4ffdc442455acf2f45695ed23591a0a959743ed5
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-05-15 17:32:18 -0300

    feat: Update components
    - package.json
    - src/components/atoms/Button/index.tsx
    - src/components/molecules/Collapsible/index.tsx
    - src/components/templates/layout.tsx
    - src/pages/dashboard/index.tsx
    - yarn.lock

--- src/components/templates/layout.tsx
+++ src/components/templates/layout.tsx
@@ -1,6 +1,7 @@
 import { signOut, useSession } from 'next-auth/react'
+import Button from '@/components/atoms/Button'
 import Image from 'next/image'
-
+import Balancer from 'react-wrap-balancer'
 interface LayoutProps {
   children?: React.ReactNode
 }
@@ -9,13 +10,14 @@ export const Layout: React.FC<LayoutProps> = ({ ...props }) => {
   const { data: session } = useSession()
 
   return (
-    <div className="min-h-screen bg-base-200 flex flex-col" {...props}>
+    <div className="min-h-screen bg-base-200 " {...props}>
       <header
         className="navbar 
           bg-gradient-to-bl from-stone-200 via-stone-300 to-stone-400
           shadow-lg"
       >
-        <section className="navbar-section flex items-center justify-between mx-10">
+        <section className="navbar-section flex items-center justify-between mx-2 sm:mx-10">
+          
           <a href="#" className="navbar-brand mr-2">
             <Image
               src="/seprem-logo.png"
@@ -24,17 +26,16 @@ export const Layout: React.FC<LayoutProps> = ({ ...props }) => {
               alt="logo seprem"
             />
           </a>
-          <div className="flex flex-col">
-            <h2 className="text-sm font-bold text-black pt-2">
-              Olá, {session?.user.name}
-            </h2>
-          </div>
-          <button
-            className="btn btn-link color-red text-2xl ml-auto mr-4"
-            onClick={() => signOut({ callbackUrl: '/' })}
-          >
-            <span className="font-bold">Sair</span>
-          </button>
+          <Balancer className="text-lg xs:text-xs font-bold text-black pt-2 mx-2 w-fit">
+            Olá, {session?.user.name}
+          </Balancer>
+          
+          <Button 
+          onClick={() => signOut({ callbackUrl: '/' })}
+          
+          color="danger">
+          Sair
+        </Button>
         </section>
       </header>
 
